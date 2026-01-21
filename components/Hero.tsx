5a5e93e2c63d98ba394ac7608aeec7380a460598
palco-zzz <yuutoooong@gmail.com>
2026-01-21 17:57:37 +0700

    fix bug. Hero images doenst shows up on prod

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero: React.FC = () => {
       {/* Dynamic Background */}
       <div className="absolute inset-0 z-0">
         <img
-          src="/assets/images/hero.webp"
+          src="/assets/images/Hero.webp"
           // src="https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?q=80&w=2021&auto=format&fit=crop"
           alt="Travel Purworejo Jakarta Landscape"
           fetchPriority="high"
